0d5cf544d7e75d17799e25ca56fab21f4284c18a
saiinteszz <italo007jose@gmail.com>
2025-05-22 11:15:25 -0300

    aqui eu vi let, console,var,  e fiz calculadora

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,17 @@
-console.log("oi")
+/*let num1 = 5;
+let num2 = 10;
+let  som = num1 + num2;
+console.log("a soma é "+som)
+console.log("______________________________________")
+let mult = num1 * num2;
+console.log("a multiplicação é "+mult)
+let sub = num1 - num2;
+console.log("______________________________________")
+let div = num1 / num2;
+console.log("a divisão é "+div)*/
+let usuario = ("italo");
+let salario = 2036;
+let meses= 12
+let trabalhados = 12
+let mult = salario/meses*trabalhados;
+console.log(usuario+" seu 13° é R$"+mult)
